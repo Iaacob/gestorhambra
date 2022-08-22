8f13cc0eb6b29e622f2a55afd0ae707aeb30ca1f
Android Studio <devandroid@ort>
2022-08-22 14:58:00 -0300

    hola

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -12,6 +12,8 @@ function Formulario({handleSubmit}) {
         sintomas: ""
     })
 
+    console.log(citaForm)
+    
     const alert =()=>{
         console.log(citaForm);
     }
@@ -23,15 +25,15 @@ function Formulario({handleSubmit}) {
                 <div className="flex-col col-6">
                     <form onSubmit={handleSubmit}>
                         <label>nombre de la mascota:</label>
-                        <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" onChange={(e) => setCitaForm({nombreMascota: e.value})}></input>
+                        <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" onChange={(e) => setCitaForm({nombreMascota: e.target.value})}></input>
                         <label>nombre del dueño:</label>
-                        <input type="text" name="dueño" class="u-full-width" placeholder="Nombre del dueño" onChange={(e) => setCitaForm({nombreDueño: e.value})}></input>
+                        <input type="text" name="dueño" class="u-full-width" placeholder="Nombre del dueño" onChange={(e) => setCitaForm({nombreDueño: e.target.value})}></input>
                         <label>fecha:</label>
-                        <input type="date" name="fecha" class="u-full-width" onChange={(e) => setCitaForm({fecha: e.value})}></input>
+                        <input type="date" name="fecha" class="u-full-width" onChange={(e) => setCitaForm({fecha: e.target.value})}></input>
                         <label>hora:</label>
-                        <input type="time" name="hora" class="u-full-width" onChange={(e) => setCitaForm({hora: e.value})}></input>
+                        <input type="time" name="hora" class="u-full-width" onChange={(e) => setCitaForm({hora: e.target.value})}></input>
                         <label>Síntomas:</label>
-                        <textarea name="sintomas" class="u-full-width" onChange={(e) => setCitaForm({sintomas: e.value})}></textarea>
+                        <textarea name="sintomas" class="u-full-width" onChange={(e) => setCitaForm({sintomas: e.target.value})}></textarea>
                     </form>
                     <button type="submit" class="u-full-width button-primary" onClick={alert}>Agregar Cita</button>
                 </div>
